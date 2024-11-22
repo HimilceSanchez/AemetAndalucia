d73938cb69844f5b054cec5ea84ad7e0cad1c919
HimilceSanchez <himisan27@gmail.com>
2024-11-22 10:36:31 +0100

    10:30 hsg

---
+++ hsgAemet.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class hsgAemet {
+
+    static int NCiudades=8;
+    String[] nombres = {"Almeria", "Granada", "Cordoba", "Jaen", "Malaga", "Sevilla", "Cadiz"};
+    static int registroTemp;
+
+     public static void main(String[] args) {
+        
+     }
+     public static void hsgGenerarRegistroTemp(){
+       for (int i=0; i<hsgRegCiudad.NDias; i++){
+        for (int j=0; j<nombres;)
+        hsgRegCiudad.temperatura[i][j]=;
+       }
+     }
+}
---
+++ hsgRegCiudad.java
@@ -0,0 +1,11 @@
+public class hsgRegCiudad {
+    static int NDias=7;
+    int TEMP=3;
+    String Nombre;
+    int tmax;
+    int tmin;
+    static String[] diasSemana= {"Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"};
+    int [][]temperatura=new int[NDias][hsgAemet.NCiudades];
+
+
+}

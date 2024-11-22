5f9e6cf1f8cd4d5187ce28269b908047823b4d9c
HimilceSanchez <himisan27@gmail.com>
2024-11-22 12:41:04 +0100

    12:00 hsg

--- hsgAemet.java
+++ hsgAemet.java
@@ -1,17 +1,47 @@
 import java.util.Scanner;
+
 public class hsgAemet {
 
-    static int NCiudades=8;
-    String[] nombres = {"Almeria", "Granada", "Cordoba", "Jaen", "Malaga", "Sevilla", "Cadiz"};
+    static int NCiudades = 8;
+    static String[] nombres = { "Almeria", "Granada", "Cordoba", "Jaen", "Malaga", "Sevilla", "Cadiz" };
     static int registroTemp;
 
-     public static void main(String[] args) {
+    public static void main(String[] args) {
+        hsgRegCiudad.hsgGenerarTemp();
+        hsgListarRegistroTemp();
+        hsgGenerarRegistroTemp();
+    }
+
+    public static void hsgGenerarRegistroTemp() {
+        System.out.printf("%20s", "");
+      
+      for(int i = 0;i<NCiudades;i++)
+    {
+        System.out.printf("%20s", nombres[i] + "   ");
+
+    }
+}
+
+    public static void hsgListarRegistroTemp(){
+        System.out.println("------ LISTADO DE Temperaturas --------------");
+        System.out.printf("%20s", "");
+        for (int i = 0; i < hsgRegCiudad.NDias; i++) {
+            System.out.printf("%10s", hsgRegCiudad.diasSemana[i] + "  ");
+            
+        }
+        System.out.println();
+        for (int i = 0; i < NCiudades; i++) {
+            System.out.printf("%20s", nombres[i] + "   ");
+            
+            for (int j = 0; j < hsgRegCiudad.NDias; j++) {
+                System.out.printf("%10.2s", hsgRegCiudad.temperatura[i][j] + "        ");
+                
+            }
+            System.out.println();
+        }
+     }
+
+    public static int hsggetDatosDia(String dia) {
         
-     }
-     public static void hsgGenerarRegistroTemp(){
-       for (int i=0; i<hsgRegCiudad.NDias; i++){
-        for (int j=0; j<nombres;)
-        hsgRegCiudad.temperatura[i][j]=;
-       }
-     }
+    }
 }
--- hsgRegCiudad.java
+++ hsgRegCiudad.java
@@ -1,11 +1,38 @@
 public class hsgRegCiudad {
     static int NDias=7;
-    int TEMP=3;
+    static int TEMP=3;
     String Nombre;
-    int tmax;
-    int tmin;
+    public static int tmax=-1;
+    public static int tmin=40;
     static String[] diasSemana= {"Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"};
-    int [][]temperatura=new int[NDias][hsgAemet.NCiudades];
+    public static int [][]temperatura=new int[NDias][hsgAemet.NCiudades];
+    public static int temp;
 
+    public static void hsgGenerarTemp(){
+        
+        for (int i=0; i<hsgRegCiudad.NDias; i++){
+        for (int j=0; j<hsgAemet.NCiudades; j++){
+         hsgRegCiudad.temperatura[i][j]=(int)(Math.random()*39)+1;
+         temp= hsgRegCiudad.temperatura[i][j];
+         
+        }
+    }
+}
+    // public static void MostrarTabla(){
+    //     System.out.println(hsgGenerarTemp());
+    // }
+
+   
+
+
+    // }
+    public static void hsggetCiudadTempMax(){
+        for(int i=0; i < temp; i++){
+            if(temp<tmax){
+                tmax=temp;
+            }
+        }
+    }
 
 }
+
